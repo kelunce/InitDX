bb37555640ae829ce45da30c83a0941ff57ce69f
kelunce <lichengman@163.comm>
2013-01-05 18:52:41 +0800

    dx

---
+++ InitDX/src/Main.cpp
@@ -0,0 +1,100 @@
+﻿//////////////////////////////////////////////////////////////////////////////////////////////////
+// 
+// File: d3dxcreate.cpp
+// 
+// Author: Frank D. Luna (C) All Rights Reserved
+//
+// System: AMD Athlon 1800+ XP, 512 DDR, Geforce 3, Windows XP, MSVC++ 7.0 
+//
+// Desc: Renders several D3DX shapes in wireframe mode and has the camera
+//       fly around the scene.  Demonstrates the D3DXCreate* functions, and
+//       demonstrates more complex transformations used to position the objects
+//       in the world and move the camera around the world.
+//          
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "d3dUtility.h"
+
+//
+// Globals
+//
+
+IDirect3DDevice9*     Device = 0; 
+
+const int Width  = 640;
+const int Height = 480;
+
+
+//
+// Framework Functions
+//
+bool Setup()
+{
+    return true;
+}
+void Cleanup()
+{
+}
+
+bool Display(float timeDelta)
+{
+    if( Device )
+    {
+        //分别用黑色和1.0填充后备表面和深度/模版缓冲区
+        Device->Clear(0, 0, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, 0x00ff03f0, 1.0f, 0);
+
+        //页面切换，显示后缓冲区内容
+        Device->Present(0, 0, 0, 0);
+    }
+    return true;
+}
+
+//
+// WndProc
+//
+LRESULT CALLBACK d3d::WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch( msg )
+    {
+    case WM_DESTROY:
+        ::PostQuitMessage(0);
+        break;
+
+    case WM_KEYDOWN:
+        if( wParam == VK_ESCAPE )
+            ::DestroyWindow(hwnd);
+        break;
+    }
+    return ::DefWindowProc(hwnd, msg, wParam, lParam);
+}
+
+//
+// WinMain
+//
+int WINAPI WinMain(HINSTANCE hinstance,
+                   HINSTANCE prevInstance, 
+                   PSTR cmdLine,
+                   int showCmd)
+{
+    if(!d3d::InitD3D(hinstance,
+        Width, Height, true, D3DDEVTYPE_HAL, &Device))
+    {
+        ::MessageBox(0, L"InitD3D() - FAILED", 0, 0);
+        return 0;
+    }
+
+    if(!Setup())
+    {
+        ::MessageBox(0, L"Setup() - FAILED", 0, 0);
+        return 0;
+    }
+
+    //设置渲染函数Display
+    d3d::EnterMsgLoop( Display );
+
+    Cleanup();
+
+    Device->Release();
+
+    return 0;
+}
