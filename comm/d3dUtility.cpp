13eb4f2c538f34d4510350e63edd7a0f5ad28c4b
kelunce <lichengman@163.comm>
2013-01-18 00:24:17 +0800

    新增5,6,7章代码
    颜色,光照,纹理

--- comm/d3dUtility.cpp
+++ comm/d3dUtility.cpp
@@ -168,4 +168,63 @@ MU_DECLSPEC int d3d::EnterMsgLoop( bool (*ptr_display)(float timeDelta) )
     return msg.wParam;
 }
 
+MU_DECLSPEC D3DLIGHT9 d3d::InitDirectionalLight( D3DXVECTOR3 *direction,D3DXCOLOR *color )
+{
+    D3DLIGHT9 light;
+    ZeroMemory(&light,sizeof(D3DLIGHT9));
+    light.Type = D3DLIGHT_DIRECTIONAL;
+    light.Ambient = *color * 0.4f;
+    light.Diffuse = *color;
+    light.Specular = *color * 0.6f;
+    light.Direction = *direction;
+    return light;
+}
 
+MU_DECLSPEC D3DLIGHT9 d3d::InitPointLight( D3DXVECTOR3 *position,D3DXCOLOR *color )
+{
+    D3DLIGHT9 light;
+    ZeroMemory(&light,sizeof(D3DLIGHT9));
+    light.Type = D3DLIGHT_POINT;
+    light.Ambient = *color * 0.4f;
+    light.Diffuse = *color;
+    light.Specular = *color * 0.6f;
+    light.Position = *position;
+    light.Range = 1000.0f;
+    light.Attenuation0 = 1.0f;
+    light.Attenuation1 = 0.0f;
+    light.Attenuation2 = 0.0f;
+    return light;
+}
+
+MU_DECLSPEC D3DLIGHT9 d3d::InitSpotLight( D3DXVECTOR3 *position,D3DXVECTOR3 *direction,D3DXCOLOR *color )
+{
+    D3DLIGHT9 light;
+    ZeroMemory(&light,sizeof(D3DLIGHT9));
+
+    light.Type      = D3DLIGHT_SPOT;
+    light.Ambient   = *color * 0.4f;
+    light.Diffuse   = *color;
+    light.Specular  = *color * 0.6f;
+    light.Position  = *position;
+    light.Direction = *direction;
+    light.Range        = 1000.0f;
+    light.Falloff      = 1.0f;
+    light.Attenuation0 = 1.0f;
+    light.Attenuation1 = 0.0f;
+    light.Attenuation2 = 0.0f;
+    light.Theta        = 0.4f;
+    light.Phi          = 0.9f;
+
+    return light;
+}
+
+MU_DECLSPEC D3DMATERIAL9 d3d::InitMtrl( D3DXCOLOR a, D3DXCOLOR d, D3DXCOLOR s, D3DXCOLOR e, float p )
+{
+    D3DMATERIAL9 mtrl;
+    mtrl.Ambient = a;
+    mtrl.Diffuse = d;
+    mtrl.Emissive = e;
+    mtrl.Power = p;
+    mtrl.Specular = s;
+    return mtrl;
+}
