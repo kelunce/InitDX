85c83eeb2ae196c37e4ca5f0c75b500689e53dde
kelunce <lichengman@163.com>
2014-06-04 10:19:56 +0800

    新增设备丢失处理

--- Cube/src/cube.cpp
+++ Cube/src/cube.cpp
@@ -46,6 +46,59 @@ struct Vertex   // 程序自定义的顶点结构
 
 const DWORD Vertex::FVF = D3DFVF_XYZ;   //指明程序使用的顶点格式 包含x,y,z轴坐标
 
+void InitDevice()
+{
+	if(!Device)
+		return;
+	//
+	// Position and aim the camera.
+	//
+
+	D3DXVECTOR3 position(0.0f, 0.0f, -5.0f);    // 设置眼睛的位置
+	D3DXVECTOR3 target(0.0f, 0.0f, 0.0f);       // 设置眼睛看的视点
+	D3DXVECTOR3 up(0.0f, 1.0f, 0.0f);           // 一般都是Y轴单位向量
+    D3DXMATRIX V;
+   
+	D3DXMatrixLookAtLH(&V, &position, &target, &up); // 获取视图矩阵(该矩阵和摄像机位置和方向相关,其实就是指定摄像机的位置),默认眼睛在世界坐标原点,眼睛往屏幕里面看(Z轴正方向)
+    
+    Device->SetTransform(D3DTS_VIEW, &V);// 设置视图矩阵,即摄像机
+    
+    /*
+    这里省略如下步骤:
+    1.设置背面裁剪方式
+    Device->SetRenderState(...);
+    2.设置光照;
+    3.裁剪不可见物品(draw时要进行);
+    */
+
+	//
+	// Set the projection matrix.
+	//
+
+    // 设置投影矩阵,把3维转为2维
+	D3DXMATRIX proj;
+	D3DXMatrixPerspectiveFovLH(             // 获得指定的投影矩阵
+			&proj,                          // [out] 投影矩阵
+			D3DX_PI * 0.5f,                 // [in] 垂直视角范围,90度(即上下45度)
+			(float)Width / (float)Height,   // [in] 宽高比
+			1.0f,                           // [in] 近面距离
+			1000.0f);                       // [in] 远面距离
+	Device->SetTransform(D3DTS_PROJECTION, &proj); // 设置投影矩阵
+
+    /*
+     这里没有设置视口变换矩阵,setviewport(...)把2维坐标转换为屏幕坐标
+     最后的光栅化(计算每个三角形内的点的颜色) 都省略了
+     光栅化一般都是硬件完成,但是我们可以设置光栅化的一些状态来影响结果
+    */
+
+	//
+	// Switch to wireframe mode.
+	//    
+	Device->SetRenderState(D3DRS_FILLMODE, D3DFILL_WIREFRAME);// 使用线框线条来渲染,这个函数可以设置很多渲染状态来影响光栅化操作
+
+}
+
+
 //
 // Framework Functions
 //
@@ -58,7 +111,7 @@ bool Setup()
 		8 * sizeof(Vertex),     // [in] 想要申请的内存大小,单位为字节
 		D3DUSAGE_WRITEONLY,     // [in] 额外属性,只读/动态大小/点图元结构/软件处理
 		Vertex::FVF,            // [in] 灵活顶点格式FVF,用于指定顶点格式
-        D3DPOOL_MANAGED,        // [in] 内存池类型,有D3DPOOL_DEFAULT(默认),D3DPOOL_MANAGED(托管),D3DPOOL_SYSTEMMEM(系统),D3DPOOL_SCRATCH                 = 3,
+        D3DPOOL_MANAGED,        // [in] 内存池类型,有D3DPOOL_DEFAULT(默认,设备时丢失手动管理),D3DPOOL_MANAGED(托管),D3DPOOL_SYSTEMMEM(系统),D3DPOOL_SCRATCH                 = 3,
 		&VB,                    // [out] 申请成功后返回IDirect3DVertexBuffer9指针
 		0);                     // [in] 无用参数,置0
 
@@ -125,51 +178,7 @@ bool Setup()
 
 	IB->Unlock();
 
-	//
-	// Position and aim the camera.
-	//
-
-	D3DXVECTOR3 position(0.0f, 0.0f, -5.0f);    // 设置眼睛的位置
-	D3DXVECTOR3 target(0.0f, 0.0f, 0.0f);       // 设置眼睛看的视点
-	D3DXVECTOR3 up(0.0f, 1.0f, 0.0f);           // 一般都是Y轴单位向量
-    D3DXMATRIX V;
-   
-	D3DXMatrixLookAtLH(&V, &position, &target, &up); // 获取视图矩阵(该矩阵和摄像机位置和方向相关,其实就是指定摄像机的位置),默认眼睛在世界坐标原点,眼睛往屏幕里面看(Z轴正方向)
-    
-    Device->SetTransform(D3DTS_VIEW, &V);// 设置视图矩阵,即摄像机
-    
-    /*
-    这里省略如下步骤:
-    1.设置背面裁剪方式
-    Device->SetRenderState(...);
-    2.设置光照;
-    3.裁剪不可见物品(draw时要进行);
-    */
-
-	//
-	// Set the projection matrix.
-	//
-
-    // 设置投影矩阵,把3维转为2维
-	D3DXMATRIX proj;
-	D3DXMatrixPerspectiveFovLH(             // 获得指定的投影矩阵
-			&proj,                          // [out] 投影矩阵
-			D3DX_PI * 0.5f,                 // [in] 垂直视角范围,90度(即上下45度)
-			(float)Width / (float)Height,   // [in] 宽高比
-			1.0f,                           // [in] 近面距离
-			1000.0f);                       // [in] 远面距离
-	Device->SetTransform(D3DTS_PROJECTION, &proj); // 设置投影矩阵
-
-    /*
-     这里没有设置视口变换矩阵,setviewport(...)把2维坐标转换为屏幕坐标
-     最后的光栅化(计算每个三角形内的点的颜色) 都省略了
-     光栅化一般都是硬件完成,但是我们可以设置光栅化的一些状态来影响结果
-    */
-
-	//
-	// Switch to wireframe mode.
-	//    
-	Device->SetRenderState(D3DRS_FILLMODE, D3DFILL_WIREFRAME);// 使用线框线条来渲染,这个函数可以设置很多渲染状态来影响光栅化操作
+	InitDevice();
 
 	return true;
 }
@@ -180,6 +189,32 @@ void Cleanup()
 	d3d::Release<IDirect3DVertexBuffer9*>(VB);
 	d3d::Release<IDirect3DIndexBuffer9*>(IB);
 }
+
+void OnLostDevice(void)
+{
+	/*
+	m_sprite->OnLostDevice();
+	m_font->OnLostDevice();
+	*/
+}
+
+
+
+void OnResetDevice(void)
+{
+	D3DPRESENT_PARAMETERS &d3dpp = d3d::GetGlobleD3DPP();
+	if(FAILED(Device->Reset(&d3dpp)))
+	{
+		return;
+	}
+
+	/*
+	m_sprite->OnResetDevice();
+	m_font->OnResetDevice();
+	*/
+	InitDevice();
+}
+
 
 bool Display(float timeDelta)
 {
@@ -233,7 +268,15 @@ bool Display(float timeDelta)
                                       12);                 // 要渲染的图元数量,这里是12个三角形
 
 		Device->EndScene();
-		Device->Present(0, 0, 0, 0);
+		HRESULT hr = Device->Present(0, 0, 0, 0);
+		if(hr == D3DERR_DEVICELOST)
+		{
+			if(Device->TestCooperativeLevel() == D3DERR_DEVICENOTRESET)
+			{
+				OnLostDevice();
+				OnResetDevice();
+			}
+		}
 	}
 	return true;
 }
